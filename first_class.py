9f6ee3ea3d993223cf98a40b31449ad7488b92c9
sushant gupta <sushantgupta4u@gmail.com>
2023-10-21 15:44:17 +0530

    first class function

--- first_class.py
+++ first_class.py
@@ -3,6 +3,26 @@ def square(x):
     return x*x
 
 # f=square(5)
-f=square
-print(square)
-print(f)
+# f=square  # we can treat function f as variable
+# print(square)
+# print(f(5))
+
+def my_app(fun,arg_list):
+    result=[]
+    for i in arg_list:
+        result.append(fun(i))
+    return result
+
+squares=my_app(square,[1,2,3,4,5])
+print(squares)
+
+#return the function
+
+
+def logger(msg):
+    def log_messgae():
+        print('Log:',msg)
+    return log_messgae
+
+log_hi=logger('Hi..!')
+log_hi()
