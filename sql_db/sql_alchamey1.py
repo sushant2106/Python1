d3eaaac443bc360f1999894401960a812b1025ff
sushant gupta <sushantgupta4u@gmail.com>
2024-02-02 09:01:51 +0530

    changes in sql

--- sql_db/sql_alchamey1.py
+++ sql_db/sql_alchamey1.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine,Column,Integer,String,or_
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-
+#hi
 engine=create_engine('sqlite:///sales.db',echo=True)
 Base=declarative_base()
 
